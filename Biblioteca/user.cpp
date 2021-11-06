e9152716e62159e4141ffac08634d2a1be443999
DraMarian <dragoiumarian11@gmail.com>
2021-11-06 16:45:52 +0200

    Create book class
    Constructors+Setters+Getters

--- Biblioteca/user.cpp
+++ Biblioteca/user.cpp
@@ -4,9 +4,3 @@ User::User()
 {
 
 }
-
-User::User(QString userName, QString passwordHash)
-{
-    this->userName=userName;
-    this->passwordHash=passwordHash;
-}
--- Biblioteca/user.h
+++ Biblioteca/user.h
@@ -1,18 +1,11 @@
 #ifndef USER_H
 #define USER_H
 
-#include <QString>
-
-
 
 class User
 {
-private:
-    QString userName;
-    QString passwordHash;
 public:
     User();
-    User(QString userName, QString passwordHash);
 };
 
 #endif // USER_H
