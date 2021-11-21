9714e5c28c2cf5a2426f0097b30ef1414fcc9ff4
DraMarian <dragoiumarian11@gmail.com>
2021-11-21 13:26:11 +0200

    Added new attributes in Book class

--- Biblioteca/book.h
+++ Biblioteca/book.h
@@ -10,12 +10,24 @@ private:
     QString author;
     QString ISBN;
     int remaingDays;
+    int ID;
+    int booksCount;
+    int originalPublicationYear;
+    QString language;
+    float averageRating;
+    QString imageURL;
 public:
     Book();
     Book(QString name,QString author,QString ISBN,int remaingDays);
     const QString &getAuthor() const;
     const QString &getISBN() const;
     const QString &getName() const;
+    const int &getID() const;
+    const int &getBooksCount() const;
+    const int &getOriginalPublicationYear() const;
+    const float &getAverageRating() const;
+    const QString &getLanguage() const;
+    const QString &getImageURL() const;
     int getRemaingDays() const;
     void setRemaingDays(int newRemaingDays);
 };
