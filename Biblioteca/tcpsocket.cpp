f7f3f8ea1ca503f1d776d73cbdf9eec746607835
DraMarian <dragoiumarian11@gmail.com>
2021-12-29 16:56:45 +0200

    Added constructors and destructor to tcpsocket class

--- Biblioteca/tcpsocket.cpp
+++ Biblioteca/tcpsocket.cpp
@@ -1,6 +1,65 @@
-#include "tcpsocket.h"
+#include "TCPSocket.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
 
-TCPSocket::TCPSocket()
+TCPSockets::TCPSockets()
 {
+    WSADATA wsaData;
 
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+    }
+    // *** Create a socket ***
+    connectSocket = INVALID_SOCKET;
+
+    // Create a SOCKET for connecting to server: IPv4, stream socket, TCP
+    connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (connectSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Error at socket: " << WSAGetLastError();
+    }
+}
+
+TCPSockets::TCPSockets(SOCKET socket)
+{
+    this->connectSocket = socket;
+}
+
+TCPSockets::~TCPSockets()
+{
+    closesocket(connectSocket);
+}
+
+void TCPSockets::ConnectToServer(const std::string& address, uint16_t port)
+{
+    addrinfo* result = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));    // memset to 0
+    hints.ai_family = AF_INET;
+
+    // *** Resolve the server address and port (can be also names like "localhost") ***
+    int iResult = getaddrinfo(address.c_str(), std::to_string(port).c_str(), &hints, &result);
+    if (iResult != 0)
+    {
+        std::cerr << "getaddrinfo failed: " << iResult;
+        freeaddrinfo(result);
+    }
+
+    // Attempt to connect to the first address returned by the call to getaddrinfo
+    iResult = connect(connectSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR)
+    {
+        closesocket(connectSocket);
+        connectSocket = INVALID_SOCKET;
+    }
+
+    freeaddrinfo(result);
+
+    if (connectSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Unable to connect to server!\n";
+    }
 }
--- Biblioteca/tcpsocket.h
+++ Biblioteca/tcpsocket.h
@@ -1,11 +1,17 @@
-#ifndef TCPSOCKET_H
-#define TCPSOCKET_H
+#pragma once
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
 
-
-class TCPSocket
+class TCPSockets
 {
 public:
-    TCPSocket();
+    TCPSockets();
+    TCPSockets(SOCKET socket);
+    ~TCPSockets();
+    void ConnectToServer(const std::string&, uint16_t);
+private:
+    SOCKET connectSocket;
 };
-
-#endif // TCPSOCKET_H
